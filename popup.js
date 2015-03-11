272be51e793ecb4149913e562c695be4f1566cf4
Pan.Li <lipan@jiecao.fm>
2015-03-11 16:19:13 +0800

    [time] bind input propertychange

--- popup.js
+++ popup.js
@@ -4,17 +4,13 @@
 
 // Search the bookmarks when entering the search keyword.
 $(function () {
-    //$('#search').change(function () {
-    //
-    //});
-
-    $('#click').click(function () {// 出对话框了
-        var timestamp = $('#timestamp').val();
-        $('#result').text(getLocalTime(timestamp));//;
-
+    $('#timestamp').bind('input propertychange', function () {
+        setTime();
     });
+    setTime();
 });
 
-function getLocalTime(nS) {
-    return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/, ' ');
+function setTime() {
+    var timestamp = new Date(parseInt($('#timestamp').val()) * 1000).toLocaleString().replace(/:\d{1,2}$/, ' ');
+    $('#result').text(timestamp);
 }
